e45362fa22df83973d67714aabc387dcd88afce2
Abohmid2030 <ahmedmoustafa559@gmail.com>
2026-08-03 01:06:36 +0300

    Update generate.js

--- api/generate.js
+++ api/generate.js
@@ -116,7 +116,7 @@ export default {
 
         try {
             const geminiResponse = await fetch(
-                'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash:generateContent',
+                'https://generativelanguage.googleapis.com/v1beta/models/gemini-2.5-flash-lite:generateContent',
                 {
                     method: 'POST',
 
